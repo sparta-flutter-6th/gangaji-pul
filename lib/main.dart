9bb8c4f1b87c53511517908b8bccd638ac6a08ee
ilsong963 <ilsong963@naver.com>
2025-05-16 18:22:37 +0900

    feat: 이미지 페이지 레이아웃 작성

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gangaji_pul/firebase_options.dart';
-import 'package:gangaji_pul/presentation/view/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
