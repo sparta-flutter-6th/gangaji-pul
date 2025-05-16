3fa4cf6210ea885759cfae89e6fda0ec1713c5b3
ilsong963 <ilsong963@naver.com>
2025-05-16 20:23:11 +0900

    fix: 폴더명 수정

--- lib/presentation/view/my_page/my_page.dart
+++ lib/presentation/view/my_page/my_page.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gangaji_pul/presentation/%08providers/auth_state_provider.dart';
+import 'package:gangaji_pul/presentation/providers/auth_state_provider.dart';
 import 'package:gangaji_pul/presentation/view/my_page/widgets/guest_my_page.dart';
 import 'package:gangaji_pul/presentation/view/my_page/widgets/logged_in_my_page.dart';
 
