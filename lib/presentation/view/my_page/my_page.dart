d8883194336544e0393766d08099f6e0835f3f68
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-05-16 21:09:52 +0900

    feat: my-page BottomNavBar연결

--- lib/presentation/view/my_page/my_page.dart
+++ lib/presentation/view/my_page/my_page.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gangaji_pul/presentation/providers/auth_state_provider.dart';
+import 'package:gangaji_pul/presentation/view/bottom_nav_bar.dart';
 import 'package:gangaji_pul/presentation/view/my_page/widgets/guest_my_page.dart';
 import 'package:gangaji_pul/presentation/view/my_page/widgets/logged_in_my_page.dart';
 
@@ -13,6 +14,11 @@ class MyPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(authStateProvider);
     log('현재 user: $user');
-    return user == null ? GuestMyPage() : LoggedInMyPage();
+    return Scaffold(
+      backgroundColor: Color(0xFFEAE3C0),
+      bottomNavigationBar: BottomNavBar(currentIndex: 2),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: user == null ? GuestMyPage() : LoggedInMyPage(),
+    );
   }
 }
--- lib/presentation/view/my_page/widgets/logged_in_my_page.dart
+++ lib/presentation/view/my_page/widgets/logged_in_my_page.dart
@@ -5,73 +5,67 @@ class LoggedInMyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0XFFEAE3C0),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 100, 20, 0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Color(0XFF7C5C42),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Center(
-                    child: Icon(Icons.camera_alt, size: 50, color: Colors.grey),
-                  ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 100, 20, 0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color(0XFF7C5C42),
+                  shape: BoxShape.circle,
                 ),
+                child: Center(
+                  child: Icon(Icons.camera_alt, size: 50, color: Colors.grey),
+                ),
+              ),
 
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'name',
-                          style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'name',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          'email',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                      Text(
+                        'email',
+                        style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                      ),
+                    ],
                   ),
                 ),
-                Column(
-                  children: [
-                    Icon(Icons.pets, size: 30, color: Color(0XFF332121)),
-                    Text('0', style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-                SizedBox(width: 20),
-                Column(
-                  children: [
-                    Icon(Icons.favorite, size: 30, color: Colors.red),
-                    Text('0', style: TextStyle(fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              height: 300,
-              child: Image.asset('assets/image/grass.png', fit: BoxFit.cover),
-            ),
-          ],
-        ),
+              ),
+              Column(
+                children: [
+                  Icon(Icons.pets, size: 30, color: Color(0XFF332121)),
+                  Text('0', style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              SizedBox(width: 20),
+              Column(
+                children: [
+                  Icon(Icons.favorite, size: 30, color: Colors.red),
+                  Text('0', style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            width: double.infinity,
+            height: 300,
+            child: Image.asset('assets/image/grass.png', fit: BoxFit.cover),
+          ),
+        ],
       ),
     );
   }
