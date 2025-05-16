5c7be9d12826afced88872b5878a3ec442ab7c20
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-05-16 18:04:58 +0900

    fix: page change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gangaji_pul/firebase_options.dart';
 import 'package:gangaji_pul/presentation/view/home_page.dart';
-import 'package:gangaji_pul/presentation/view/my_page/my_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +16,6 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(home: MyPage());
+    return MaterialApp(home: HomePage());
   }
 }
