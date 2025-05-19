655f60fe108871803bae22771bef13586f3e1451
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-05-19 15:17:29 +0900

    refactor: 불필요한 패키지 제거

--- lib/presentation/view/my_page/widgets/guest_my_page.dart
+++ lib/presentation/view/my_page/widgets/guest_my_page.dart
@@ -1,11 +1,8 @@
 import 'dart:developer';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gangaji_pul/presentation/providers/auth_state_provider.dart';
-import 'package:gangaji_pul/service/auth_service_impl.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class GuestMyPage extends StatelessWidget {
   const GuestMyPage({super.key});
