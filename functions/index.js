3852a2573901f7636cb80b43927d06e56890c266
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-05-20 18:34:22 +0900

    feat: 글 작성시 postCount 1 증가

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,14 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 
-exports.onPostCreate = functions.firestore
-    .document("posts/{postId}")
-    .onCreate((snap, context) => {
-        const newPost = snap.data();
-        const uid = newPost.uid;
-        console.log("새 게시물 작성자 UID:", uid);
+initializeApp();
 
-        // 예: 알림 보내기, 로깅, 다른 문서 업데이트 등 가능
-        return null;
-    });
-
-exports.onPostDelete = functions.firestore
-    .document('posts/{postId}')
-    .onDelete((snap, context) => {
-        const deletedPost = snap.data();
-        const uid = deletedPost.uid;
-        console.log('삭제된 게시물 작성자 UID:', uid);
-
-        // 삭제 후 처리할 작업 추가 가능 (예: 통계 감소, 알림 등)
-
-        return null;
-    });
+exports.countPost = onDocumentCreated("/posts/{postId}", async (event) => {
+  const uid = event.data.data().uid;
+  const docRef = getFirestore().collection("users").doc(uid);
+  const doc = await docRef.get();
+  docRef.update({
+    "postCount": doc.data().postCount + 1,
+  });
+});
