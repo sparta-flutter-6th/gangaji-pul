480b14e0782521ac93db649884860af690d70e21
jinju9513 <wjswlswn99@naver.com>
2025-05-20 15:49:36 +0900

    feat : post_provider

--- lib/data/providers/post_repository_provider.dart
+++ lib/data/providers/post_repository_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gangaji_pul/data/providers/post_data_source_provider.dart';
 import 'package:gangaji_pul/data/repository/post_repository_impl.dart';
@@ -5,5 +6,6 @@ import 'package:gangaji_pul/domain/repository/post_repository.dart';
 
 final postRepositoryProvider = Provider<PostRepository>((ref) {
   final dataSource = ref.read(postDataSourceProvider);
-  return PostRepositoryImpl(dataSource);
+  final firestore = FirebaseFirestore.instance;
+  return PostRepositoryImpl(dataSource, firestore);
 });
