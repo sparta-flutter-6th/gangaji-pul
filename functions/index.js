c138bf28c00ecf80676cb964cd178a99b995158c
ilsong963 <ilsong963@naver.com>
2025-05-22 10:41:20 +0900

    feat: firebase function 업로드

--- functions/index.js
+++ functions/index.js
@@ -1,84 +1,110 @@
-const {onDocumentCreated, onDocumentDeleted} =
- require("firebase-functions/v2/firestore");
-const {initializeApp} = require("firebase-admin/app");
-const {getFirestore} = require("firebase-admin/firestore");
+const { onDocumentCreated, onDocumentDeleted } =
+  require("firebase-functions/v2/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
 const db = getFirestore();
 
 exports.incrementUserPostCount =
-onDocumentCreated("/posts/{postId}", async (event) => {
-  const uid = event.data.data().userId;
-  const docRef = db.collection("users").doc(uid);
-  const doc = await docRef.get();
-  await docRef.update({
-    "postCount": doc.data().postCount + 1,
-  });
-  const snapshot = await db.collection("users")
+  onDocumentCreated("/posts/{postId}", async (event) => {
+    const uid = event.data.data().userId;
+    const docRef = db.collection("users").doc(uid);
+    const doc = await docRef.get();
+    await docRef.update({
+      "postCount": doc.data().postCount + 1,
+    });
+    const snapshot = await db.collection("users")
       .orderBy("postCount", "desc")
       .limit(3)
       .get();
-  const topUsersByPostCount = snapshot.docs.map((doc) => ({
-    uid: doc.id,
-    ...doc.data(),
-  }));
-  await db.collection("topUsers").doc("topUsersByPostCount").set({
-    "users": [...topUsersByPostCount]}, {merge: true});
-});
+    const topUsersByPostCount = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    await db.collection("topUsers").doc("topUsersByPostCount").set({
+      "users": [...topUsersByPostCount]
+    }, { merge: true });
+  });
 exports.decrementUserPostCount =
-onDocumentDeleted("/posts/{postId}", async (event) => {
-  const uid = event.data.data().userId;
-  const docRef = db.collection("users").doc(uid);
-  const doc = await docRef.get();
-  await docRef.update({
-    "postCount": doc.data().postCount - 1,
-  });
-  const snapshot = await db.collection("users")
+  onDocumentDeleted("/posts/{postId}", async (event) => {
+    const uid = event.data.data().userId;
+    const docRef = db.collection("users").doc(uid);
+    const doc = await docRef.get();
+    await docRef.update({
+      "postCount": doc.data().postCount - 1,
+    });
+    const snapshot = await db.collection("users")
       .orderBy("postCount", "desc")
       .limit(3)
       .get();
-  const topUsersByPostCount = snapshot.docs.map((doc) => ({
-    uid: doc.id,
-    ...doc.data(),
-  }));
-  await db.collection("topUsers").doc("topUsersByPostCount").set({
-    "users": [...topUsersByPostCount]}, {merge: true});
-});
+    const topUsersByPostCount = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    await db.collection("topUsers").doc("topUsersByPostCount").set({
+      "users": [...topUsersByPostCount]
+    }, { merge: true });
+  });
 exports.incrementUserlikeCount =
-onDocumentCreated("/posts/{postId}/likes/{userId}", async (event)=>{
-  const {userId} = event.params;
-  const docRef = db.collection("users").doc(userId);
-  const user = await docRef.get();
-  await docRef.update({
-    "likeCount": user.data().likeCount + 1,
-  });
-  const snapshot = await db.collection("users")
+  onDocumentCreated("/posts/{postId}/likes/{userId}", async (event) => {
+    const { userId } = event.params;
+    const docRef = db.collection("users").doc(userId);
+    const user = await docRef.get();
+    await docRef.update({
+      "likeCount": user.data().likeCount + 1,
+    });
+    const snapshot = await db.collection("users")
       .orderBy("likeCount", "desc")
       .limit(3)
       .get();
-  const topUsersBylikeCount = snapshot.docs.map((doc) => ({
-    uid: doc.id,
-    ...doc.data(),
-  }));
-  await db.collection("topUsers").doc("topUsersBylikeCount").set({
-    "users": [...topUsersBylikeCount]}, {merge: true});
-});
+    const topUsersBylikeCount = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    await db.collection("topUsers").doc("topUsersBylikeCount").set({
+      "users": [...topUsersBylikeCount]
+    }, { merge: true });
+  });
+
 exports.decrementUserlikeCount =
-onDocumentDeleted("/posts/{postId}/likes/{userId}", async (event)=>{
-  const {userId} = event.params;
-  const docRef = db.collection("users").doc(userId);
-  const user = await docRef.get();
-  await docRef.update({
-    "likeCount": user.data().likeCount > 0 ? user.data().likeCount- 1 : 0,
-  });
-  const snapshot = await db.collection("users")
+  onDocumentDeleted("/posts/{postId}/likes/{userId}", async (event) => {
+    const { userId } = event.params;
+    const docRef = db.collection("users").doc(userId);
+    const user = await docRef.get();
+    await docRef.update({
+      "likeCount": user.data().likeCount > 0 ? user.data().likeCount - 1 : 0,
+    });
+    const snapshot = await db.collection("users")
       .orderBy("likeCount", "desc")
       .limit(3)
       .get();
-  const topUsersBylikeCount = snapshot.docs.map((doc) => ({
-    uid: doc.id,
-    ...doc.data(),
-  }));
-  await db.collection("topUsers").doc("topUsersBylikeCount").set({
-    "users": [...topUsersBylikeCount]}, {merge: true});
-});
+    const topUsersBylikeCount = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    await db.collection("topUsers").doc("topUsersBylikeCount").set({
+      "users": [...topUsersBylikeCount]
+    }, { merge: true });
+  });
+
+exports.incrementUserChatCount =
+  onDocumentCreated("/chats/{userId}", async (event) => {
+    const { userId } = event.params;
+    const docRef = db.collection("users").doc(userId);
+    const user = await docRef.get();
+    await docRef.update({
+      "chatCount": user.data().likeCount + 1,
+    });
+    const snapshot = await db.collection("users")
+      .orderBy("chatCount", "desc")
+      .limit(3)
+      .get();
+    const topUsersByChatCount = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      ...doc.data(),
+    }));
+    await db.collection("topUsers").doc("topUsersByChatCount").set({
+      "users": [...topUsersByChatCount]
+    }, { merge: true });
+  });
